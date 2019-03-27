05d2e7e6198f66d630eb810b0344d4d3636e3cd4
Eternity86 <F0HTp[W{p:3!Y6>
2019-03-28 02:26:31 +0300

    правки по коду

--- src/ru/aChat/home/client/Controller.java
+++ src/ru/aChat/home/client/Controller.java
@@ -52,26 +52,20 @@ public class Controller {
         this.isAuthorized = isAuthorized;
 
         if(!isAuthorized) {
-            Platform.runLater(new Runnable() {
-                @Override
-                public void run() {
-                    upperPanel.setVisible(true);
-                    upperPanel.setManaged(true);
-                    bottomPanel.setVisible(false);
-                    bottomPanel.setManaged(false);
-                    labelUserCount.setText("0");
-                    userList.getItems().clear();
-                }
+            Platform.runLater(() -> {
+                upperPanel.setVisible(true);
+                upperPanel.setManaged(true);
+                bottomPanel.setVisible(false);
+                bottomPanel.setManaged(false);
+                labelUserCount.setText("0");
+                userList.getItems().clear();
             });
         } else {
-            Platform.runLater(new Runnable() {
-                @Override
-                public void run() {
-                    upperPanel.setVisible(false);
-                    upperPanel.setManaged(false);
-                    bottomPanel.setVisible(true);
-                    bottomPanel.setManaged(true);
-                }
+            Platform.runLater(() -> {
+                upperPanel.setVisible(false);
+                upperPanel.setManaged(false);
+                bottomPanel.setVisible(true);
+                bottomPanel.setManaged(true);
             });
         }
     }
@@ -82,69 +76,63 @@ public class Controller {
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
 
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        // цикл для авторизации
-                        while (true) {
+            new Thread(() -> {
+                try {
+                    // цикл для авторизации
+                    while (true) {
+                        String str = in.readUTF();
+                        if(str.startsWith("/authok")) {
+                            setAuthorized(true);
+                            break;
+                        } else {
+                            textArea.appendText(String.format("%s%s", str, System.lineSeparator()));
+                        }
+                    }
+                    // цикл для работы
+                    while (true) {
+                        if(!socket.isClosed()) {
                             String str = in.readUTF();
-                            if(str.startsWith("/authok")) {
-                                setAuthorized(true);
-                                break;
+                            if (str.startsWith("/")) {
+                                if (str.equals("/serverClosed")) {
+                                    setAuthorized(false);
+                                    break;
+                                }
+                                if(str.startsWith("/clientlist")) {
+                                    String[] tokens = str.split(" ");
+                                    Platform.runLater(() -> {
+                                        userList.getItems().clear();
+                                        labelUserCount.setText(String.valueOf(tokens.length - 1));
+                                        userList.getItems().clear();
+                                        for (int i = 1; i < tokens.length; i++) {
+                                            userList.getItems().add(tokens[i]);
+                                        }
+                                    });
+                                }
                             } else {
                                 textArea.appendText(String.format("%s%s", str, System.lineSeparator()));
                             }
                         }
-                        // цикл для работы
-                        while (true) {
-                            if(!socket.isClosed()) {
-                                String str = in.readUTF();
-                                if (str.startsWith("/")) {
-                                    if (str.equals("/serverClosed")) {
-                                        setAuthorized(false);
-                                        break;
-                                    }
-                                    if(str.startsWith("/clientlist")) {
-                                        String[] tokens = str.split(" ");
-                                        Platform.runLater(new Runnable() {
-                                            @Override
-                                            public void run() {
-                                                userList.getItems().clear();
-                                                labelUserCount.setText(String.valueOf(tokens.length - 1));
-                                                userList.getItems().clear();
-                                                for (int i = 1; i < tokens.length; i++) {
-                                                    userList.getItems().add(tokens[i]);
-                                                }
-                                            }
-                                        });
-                                    }
-                                } else {
-                                    textArea.appendText(String.format("%s%s", str, System.lineSeparator()));
-                                }
-                            }
-                        }
-                    } catch (IOException e) {
-                        System.out.println(e.getMessage());;
-                        //e.printStackTrace();
-                    } finally {
-                        try {
-                            out.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        try {
-                            in.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        try {
-                            socket.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        setAuthorized(false);
                     }
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());;
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    setAuthorized(false);
                 }
             }).start();
         } catch (IOException e) {
@@ -174,12 +162,9 @@ public class Controller {
         if (!textField.getText().equals("")) {
             try {
                 out.writeUTF(textField.getText());
-                Platform.runLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        textField.clear();
-                        textField.requestFocus();
-                    }
+                Platform.runLater(() -> {
+                    textField.clear();
+                    textField.requestFocus();
                 });
             } catch (IOException e) {
                 e.printStackTrace();
