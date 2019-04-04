2ca2cc19eef89ba6d30665678468aa18e8e4ae0e
Infinity2010 <arbiter2008@ya.ru>
2019-04-04 12:39:41 +0300

    использование вместо Thread ExecutorService c cjplfybtv 5 gjnjrjd

--- src/ru/aChat/home/server/ClientHandler.java
+++ src/ru/aChat/home/server/ClientHandler.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.List;
 import java.util.Vector;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 class ClientHandler {
 
@@ -41,8 +44,9 @@ class ClientHandler {
             this.in = new DataInputStream(socket.getInputStream());
             this.out = new DataOutputStream(socket.getOutputStream());
             this.blackList = new Vector<>();
+            ExecutorService executorService = Executors.newFixedThreadPool(5);
 
-            new Thread(() -> {
+            executorService.submit(() -> {
                 try {
                     // цикл для авторизации
                     while (true) {
@@ -114,7 +118,82 @@ class ClientHandler {
                     server.broadCastMsg(ClientHandler.this, nick + " покинул чат");
                     System.out.println(nick + " покинул чат");
                 }
-            }).start();
+            });
+            executorService.shutdown();
+
+//            new Thread(() -> {
+//                try {
+//                    // цикл для авторизации
+//                    while (true) {
+//                        String str = in.readUTF();
+//                        if(str.startsWith("/auth")) {
+//                            String[] tokens = str.split(" ");
+//                            String newNick = AuthService.getNickByLoginAndPass(tokens[1], tokens[2]);
+//                            if(newNick != null) {
+//                                if (isNickBusy(server.getClients(), newNick)) {
+//                                    sendMsg("Учётная запись уже используется!");
+//                                } else {
+//                                    nick = newNick;
+//                                    sendMsg("/authok " + nick);
+//                                    server.subscribe(ClientHandler.this);
+//                                    server.broadCastMsg(ClientHandler.this, nick + " зашёл в чат");
+//                                    break;
+//                                }
+//                            } else {
+//                                sendMsg("Неверный логин/пароль!");
+//                            }
+//                        }
+//                    }
+//                    // цикл для работы
+//                    while (true) {
+//                        String str = in.readUTF();
+//                        // блок служебных команд
+//                        if (str.startsWith("/")) {
+//                            if(str.equals("/end")) {
+//                                out.writeUTF("/serverClosed");
+//                                break;
+//                            }
+//                            if (str.startsWith("/w ")) {
+//                                String[] tokens = str.split("\\s", 3);
+//                                server.privateMsg(this, tokens[1], tokens[2]);
+//                            }
+//                            if (str.startsWith("/blacklist ")) {
+//                                String[] tokens = str.split("\\s");
+//                                if (checkBlackList(tokens[1])) {
+//                                    sendMsg("Пользователь " + tokens[1] + " уже в чёрном списке!");
+//                                } else {
+//                                    blackList.add(tokens[1]);
+//                                    sendMsg("Вы добавили пользователя " + tokens[1] + " в черный список!");
+//                                }
+//                            }
+//                        } else {
+//                            server.broadCastMsg(ClientHandler.this,nick + ": " + str);
+//                            System.out.println(nick + ": " + str);
+//                        }
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                } finally {
+//                    try {
+//                        in.close();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                    try {
+//                        out.close();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                    try {
+//                        socket.close();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                    server.unsubscribe(ClientHandler.this);
+//                    server.broadCastMsg(ClientHandler.this, nick + " покинул чат");
+//                    System.out.println(nick + " покинул чат");
+//                }
+//           }).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
