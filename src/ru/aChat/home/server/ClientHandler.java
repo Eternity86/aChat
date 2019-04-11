8e0e25d700bcb5dfd0c2220f0d73a0c8767294df
Infinity2010 <arbiter2008@ya.ru>
2019-04-11 16:20:54 +0300

    Добавлен локальный логгер обработчика клиента на сервере (пока без записи в файл на диске)

--- src/ru/aChat/home/server/ClientHandler.java
+++ src/ru/aChat/home/server/ClientHandler.java
@@ -5,12 +5,14 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.List;
+import java.util.Objects;
 import java.util.Vector;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Logger;
 
 class ClientHandler {
+    private static final Logger CLIENT_LOGGER = Logger.getLogger(ClientHandler.class.getName());
 
     private MainServer server;
     private Socket socket;
@@ -28,7 +30,7 @@ class ClientHandler {
         return blackList.contains(nick);
     }
 
-    private boolean isNickBusy (Vector<ClientHandler> vch, String nick) {
+    private boolean isNickBusy(Vector<ClientHandler> vch, String nick) {
         for (ClientHandler ch : vch) {
             if (ch.getNick().equals(nick)) {
                 return true;
@@ -44,17 +46,20 @@ class ClientHandler {
             this.in = new DataInputStream(socket.getInputStream());
             this.out = new DataOutputStream(socket.getOutputStream());
             this.blackList = new Vector<>();
-            ExecutorService executorService = Executors.newFixedThreadPool(5);
 
-            executorService.submit(() -> {
+            ExecutorService executorService = Executors.newFixedThreadPool(6);
+            executorService.execute(() -> {
                 try {
                     // цикл для авторизации
                     while (true) {
-                        String str = in.readUTF();
-                        if(str.startsWith("/auth")) {
+                        String str = null;
+                        if (!socket.isClosed()) {
+                            str = in.readUTF();
+                        }
+                        if (Objects.requireNonNull(str).startsWith("/auth")) {
                             String[] tokens = str.split(" ");
                             String newNick = AuthService.getNickByLoginAndPass(tokens[1], tokens[2]);
-                            if(newNick != null) {
+                            if (newNick != null) {
                                 if (isNickBusy(server.getClients(), newNick)) {
                                     sendMsg("Учётная запись уже используется!");
                                 } else {
@@ -69,13 +74,18 @@ class ClientHandler {
                             }
                         }
                     }
+
                     // цикл для работы
                     while (true) {
-                        String str = in.readUTF();
+                        String str = null;
+                        if (!socket.isClosed()) {
+                            str = in.readUTF();
+                        }
                         // блок служебных команд
-                        if (str.startsWith("/")) {
-                            if(str.equals("/end")) {
+                        if (Objects.requireNonNull(str).startsWith("/")) {
+                            if (str.equals("/end")) {
                                 out.writeUTF("/serverClosed");
+                                CLIENT_LOGGER.info("Клиент закрыл соединение с сервером");
                                 break;
                             }
                             if (str.startsWith("/w ")) {
@@ -92,8 +102,8 @@ class ClientHandler {
                                 }
                             }
                         } else {
-                            server.broadCastMsg(ClientHandler.this,nick + ": " + str);
-                            System.out.println(nick + ": " + str);
+                            server.broadCastMsg(ClientHandler.this, nick + ": " + str);
+                            CLIENT_LOGGER.info(nick + ": " + str);
                         }
                     }
                 } catch (IOException e) {
@@ -116,84 +126,10 @@ class ClientHandler {
                     }
                     server.unsubscribe(ClientHandler.this);
                     server.broadCastMsg(ClientHandler.this, nick + " покинул чат");
-                    System.out.println(nick + " покинул чат");
+                    CLIENT_LOGGER.info(nick + " покинул чат");
                 }
             });
             executorService.shutdown();
-
-//            new Thread(() -> {
-//                try {
-//                    // цикл для авторизации
-//                    while (true) {
-//                        String str = in.readUTF();
-//                        if(str.startsWith("/auth")) {
-//                            String[] tokens = str.split(" ");
-//                            String newNick = AuthService.getNickByLoginAndPass(tokens[1], tokens[2]);
-//                            if(newNick != null) {
-//                                if (isNickBusy(server.getClients(), newNick)) {
-//                                    sendMsg("Учётная запись уже используется!");
-//                                } else {
-//                                    nick = newNick;
-//                                    sendMsg("/authok " + nick);
-//                                    server.subscribe(ClientHandler.this);
-//                                    server.broadCastMsg(ClientHandler.this, nick + " зашёл в чат");
-//                                    break;
-//                                }
-//                            } else {
-//                                sendMsg("Неверный логин/пароль!");
-//                            }
-//                        }
-//                    }
-//                    // цикл для работы
-//                    while (true) {
-//                        String str = in.readUTF();
-//                        // блок служебных команд
-//                        if (str.startsWith("/")) {
-//                            if(str.equals("/end")) {
-//                                out.writeUTF("/serverClosed");
-//                                break;
-//                            }
-//                            if (str.startsWith("/w ")) {
-//                                String[] tokens = str.split("\\s", 3);
-//                                server.privateMsg(this, tokens[1], tokens[2]);
-//                            }
-//                            if (str.startsWith("/blacklist ")) {
-//                                String[] tokens = str.split("\\s");
-//                                if (checkBlackList(tokens[1])) {
-//                                    sendMsg("Пользователь " + tokens[1] + " уже в чёрном списке!");
-//                                } else {
-//                                    blackList.add(tokens[1]);
-//                                    sendMsg("Вы добавили пользователя " + tokens[1] + " в черный список!");
-//                                }
-//                            }
-//                        } else {
-//                            server.broadCastMsg(ClientHandler.this,nick + ": " + str);
-//                            System.out.println(nick + ": " + str);
-//                        }
-//                    }
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                } finally {
-//                    try {
-//                        in.close();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                    try {
-//                        out.close();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                    try {
-//                        socket.close();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                    server.unsubscribe(ClientHandler.this);
-//                    server.broadCastMsg(ClientHandler.this, nick + " покинул чат");
-//                    System.out.println(nick + " покинул чат");
-//                }
-//           }).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -204,6 +140,7 @@ class ClientHandler {
             out.writeUTF(msg);
         } catch (IOException e) {
             e.printStackTrace();
+            CLIENT_LOGGER.warning("Ошибка при отправке сообщения пользователю(ям)");
         }
     }
 
